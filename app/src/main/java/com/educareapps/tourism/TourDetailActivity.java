ee0a384b59323d7ab95bf75cee38c4042a977a4e
Ibrar Tanim Rokan <ibrar.tanim@gmail.com>
2017-07-11 17:53:10 +0600

    Minor change

--- app/src/main/java/com/educareapps/tourism/TourDetailActivity.java
+++ app/src/main/java/com/educareapps/tourism/TourDetailActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.educareapps.mylibrary.BaseActivity;
 
@@ -11,6 +14,9 @@ public class TourDetailActivity extends BaseActivity {
     ImageButton ibtnDetailBack;
     TourDetailActivity activity;
     int tourPic;
+    ImageView ivtourDetailPic;
+    TextView tvDetailTitle, tvDetailDuration, tvDetailExplain;
+    RatingBar rtBarDetail;
     String tourTitle, duration, detail;
 
     @Override
@@ -20,8 +26,22 @@ public class TourDetailActivity extends BaseActivity {
 
         activity = this;
         ibtnDetailBack = (ImageButton) findViewById(R.id.ibtnDetailBack);
+        ivtourDetailPic = (ImageView) findViewById(R.id.ivtourDetailPic);
+        tvDetailTitle = (TextView) findViewById(R.id.tvDetailTitle);
+        tvDetailDuration = (TextView) findViewById(R.id.tvDetailDuration);
+        tvDetailExplain = (TextView) findViewById(R.id.tvDetailExplain);
+        rtBarDetail = (RatingBar) findViewById(R.id.rtBarDetail);
 
-        //tourPic = getIntent().getIntExtra(tourPic,0);
+
+        /*tourPic = getIntent().getIntExtra("tourPic",0);
+        tourTitle = getIntent().getStringExtra("tourTitle");
+        duration = getIntent().getStringExtra("duration");
+        detail = getIntent().getStringExtra("detail");*/
+
+        /*ivtourDetailPic.setImageResource(tourPic);
+        tvDetailTitle.setText(tourTitle);
+        tvDetailDuration.setText(duration);
+        tvDetailExplain.setText(detail);*/
 
 
         ibtnDetailBack.setOnClickListener(new View.OnClickListener() {
