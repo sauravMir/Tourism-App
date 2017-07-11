807a0f7a26459d2f8f1efb40348e217f95a96437
RK Reaz <skresat108@gmail.com>
2017-07-11 18:20:58 +0600

    Slider Added

--- app/src/main/java/com/educareapps/tourism/TourDetailActivity.java
+++ app/src/main/java/com/educareapps/tourism/TourDetailActivity.java
@@ -2,22 +2,34 @@ package com.educareapps.tourism;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.educareapps.mylibrary.BaseActivity;
 
-public class TourDetailActivity extends BaseActivity {
+import java.util.HashMap;
+
+public class TourDetailActivity extends BaseActivity  implements  BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
     ImageButton ibtnDetailBack;
     TourDetailActivity activity;
     int tourPic;
-    ImageView ivtourDetailPic;
+
     TextView tvDetailTitle, tvDetailDuration, tvDetailExplain;
     RatingBar rtBarDetail;
     String tourTitle, duration, detail;
+    SliderLayout mDemoSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +37,19 @@ public class TourDetailActivity extends BaseActivity {
         setContentView(R.layout.activity_tour_detail);
 
         activity = this;
+
         ibtnDetailBack = (ImageButton) findViewById(R.id.ibtnDetailBack);
-        ivtourDetailPic = (ImageView) findViewById(R.id.ivtourDetailPic);
+        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
         tvDetailTitle = (TextView) findViewById(R.id.tvDetailTitle);
         tvDetailDuration = (TextView) findViewById(R.id.tvDetailDuration);
         tvDetailExplain = (TextView) findViewById(R.id.tvDetailExplain);
         rtBarDetail = (RatingBar) findViewById(R.id.rtBarDetail);
+
+        HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
 
 
         /*tourPic = getIntent().getIntExtra("tourPic",0);
@@ -52,5 +71,56 @@ public class TourDetailActivity extends BaseActivity {
             }
         });
 
+
+
+        for(String name : url_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(url_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            mDemoSlider.addSlider(textSliderView);
+        }
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(4000);
+        mDemoSlider.addOnPageChangeListener(this);
+
+
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(this,slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.d("Slider Demo", "Page Changed: " + position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+    @Override
+    protected void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
     }
 }
