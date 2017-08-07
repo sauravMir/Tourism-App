3f098cddf5bb2daba8d61f679b2067f334e1cb3d
Ibrar Tanim Rokan <ibrar.tanim@gmail.com>
2017-08-07 16:20:20 +0600

    minor change

--- app/src/main/java/com/educareapps/tourism/MainActivity.java
+++ app/src/main/java/com/educareapps/tourism/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -57,6 +58,7 @@ public class MainActivity extends BaseActivity {
     //Tourism
     MainFragment mainFragment;
     boolean isDrawerOpen = false;
+    LinearLayout llOverlay;
 
 
     @Override
@@ -68,6 +70,7 @@ public class MainActivity extends BaseActivity {
         mActivityTitle = getTitle().toString();
         progressDialog = new ProgressDialog(activity);
         mainFragment = new MainFragment();
+        llOverlay = (LinearLayout) findViewById(R.id.llOverlay);
 
         addDrawerItems();
         setupDrawer();
@@ -87,14 +90,16 @@ public class MainActivity extends BaseActivity {
 
 
     private void addDrawerItems() {
+
         if(InternetAvailabilityCheck.getConnectivityStatus(activity) != StaticAccess.TYPE_NOT_CONNECTED){
             //get reference of the ExpandableListView
             simpleExpandableListView = (ExpandableListView) findViewById(R.id.simpleExpandableListView);
             showProgress();
             makeRequest(RootUrl.RootUrl + "getCountryList");
+            llOverlay.setVisibility(View.INVISIBLE);
         }
         else {
-            Toast.makeText(activity, "Net connect first", Toast.LENGTH_SHORT).show();
+            llOverlay.setVisibility(View.VISIBLE);
         }
     }
 
